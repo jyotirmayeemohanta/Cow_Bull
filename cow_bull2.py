dc67adfea1c7a0b9cb954dc7a04b7411b6c2c182
jyotirmayeemohanta <97024151+jyotirmayeemohanta@users.noreply.github.com>
2022-08-29 21:42:31 +0530

    Add files via upload

---
+++ cow_bull2.py
@@ -0,0 +1,40 @@
+import random
+
+name=input("hello enter your name :-")
+print("Welcome ",name)
+number=[]
+attempts=0
+
+def MakeNumber():
+    for i in range(4):
+        x=random.randrange(0,9)
+        number.append(x)
+    if len(number)>len(set(number)):
+        number.clear()
+        MakeNumber()
+
+def PlayGame():
+    global attempts
+    attempts +=1
+    cows = 0
+    bulls = 0
+    print(number)
+    choice = input("Please enter a 4 digit number:-")
+    guess = []
+    for i in range (4):
+        guess.append(int(choice[i]))
+        for j in range(4):
+            if guess[i] == number[j]:
+                cows +=1
+    for x in range (4):
+        if guess[x] == number[x]:
+            bulls +=1
+    print("Bulls:",bulls)
+    print("Cows:",cows)
+    if (bulls == 4):
+        print("..**.Congratulation.**..You Won after ",attempts, "attempts!")
+    if(bulls !=4):
+        PlayGame()
+        
+MakeNumber()
+PlayGame()
